fe675442186af13a0417d33831f65992f3eb3640
Ian <iangonsalves29@gmail.com>
2025-06-03 16:40:36 +0400

    fix for run php file

--- backend/start.sh
+++ backend/start.sh
@@ -3,8 +3,5 @@
 # Echo a message to indicate the script started
 echo "Startup script started."
 
-# Check if supervisord exists and show its details
-ls -l /usr/sbin/supervisord
-
 # Start Supervisor with the specified configuration (using the correct path found during build)
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf 
