6c027466949005d11a6d9f779a32a88cdd7a2e96
Ian <iangonsalves29@gmail.com>
2025-06-03 15:54:13 +0400

    runtime error log

--- backend/start.sh
+++ backend/start.sh
@@ -3,5 +3,8 @@
 # Echo a message to indicate the script started
 echo "Startup script started."
 
+# Check if supervisord exists and show its details
+ls -l /usr/sbin/supervisord
+
 # Start Supervisor with the specified configuration
 exec /usr/sbin/supervisord -c /etc/supervisor/conf.d/supervisord.conf 
