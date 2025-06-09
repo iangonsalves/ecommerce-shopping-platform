f40ce9c878f4ef96463bfae98aa84e26671ae431
Ian <iangonsalves29@gmail.com>
2025-06-09 16:55:04 +0400

    Updated start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -3,15 +3,12 @@
 # Echo a message to indicate the script started
 echo "Startup script started."
 
-# Run Laravel optimization, migrations, and seeding at runtime
-echo "Running Laravel optimize:clear..."
-php artisan optimize:clear
-
+# Run Laravel migrations and optimization at runtime
 echo "Running Laravel migrations..."
 php artisan migrate --force
 
-echo "Running Laravel database seeders... (REMOVE AFTER INITIAL SEEDING)"
-php artisan db:seed
+echo "Running Laravel optimize:clear..."
+php artisan optimize:clear
 
 # Start Supervisor with the specified configuration (using the correct path found during build)
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf 
