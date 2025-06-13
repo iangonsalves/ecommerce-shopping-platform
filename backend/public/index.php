46b6b8fc3524f5a47f9d73ec880c4f368b89125c
Ian <iangonsalves29@gmail.com>
2025-06-13 14:19:25 +0400

    update logging

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,18 +3,28 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
+try {
+    define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+    // Determine if the application is in maintenance mode...
+    if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+        require $maintenance;
+    }
+
+    // Register the Composer autoloader...
+    require __DIR__.'/../vendor/autoload.php';
+
+    // Bootstrap Laravel and handle the request...
+    /** @var Application $app */
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+
+    $response = $app->handleRequest(Request::capture());
+
+    $response->send();
+
+    $app->terminate();
+} catch (Throwable $e) {
+    error_log("Caught Fatal Exception in index.php: " . $e->getMessage() . "\n" . $e->getTraceAsString(), 3, "/dev/stderr");
+    http_response_code(500);
+    echo "500 Server Error: An unhandled exception occurred.";
 }
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
