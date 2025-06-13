90899b710ec4ae0c6a88f62016e826428b9b55e4
Ian <iangonsalves29@gmail.com>
2025-06-13 10:15:27 +0400

    update cache  clearing

--- backend/start.sh
+++ backend/start.sh
@@ -7,8 +7,5 @@ echo "Startup script started."
 echo "Running Laravel migrations..."
 php artisan migrate --force
 
-echo "Running Laravel optimize:clear..."
-php artisan optimize:clear
-
 # Start Supervisor with the specified configuration (using the correct path found during build)
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf 
