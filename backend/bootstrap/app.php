3db099e382d35b0d6798e1ca8b2590d96a139b0a
Ian <iangonsalves29@gmail.com>
2025-06-13 15:08:17 +0400

    dd

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+dd('Reached Application::configure'); // TEMPORARY DEBUG LINE
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
