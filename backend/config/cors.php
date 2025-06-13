bcb389d08cf6defde8a71a0b0614028d4961d492
Ian <iangonsalves29@gmail.com>
2025-06-13 17:28:31 +0400

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8000'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8000', 'https://alexjerseyhubbackend.onrender.com', 'https://alexjerseyhubfrontend.onrender.com'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
