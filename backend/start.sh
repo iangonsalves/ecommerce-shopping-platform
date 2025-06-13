242df7268e09f0f2f6817f6f43e7c487b7cbb3e4
Ian <iangonsalves29@gmail.com>
2025-06-13 12:59:07 +0400

    revert

--- backend/start.sh
+++ backend/start.sh
@@ -7,9 +7,5 @@ echo "Startup script started."
 echo "Running Laravel migrations..."
 php artisan migrate --force
 
-# TEMPORARY DEBUG: Check Laravel version to force early boot output
-echo "Checking Laravel version..."
-php artisan --version
-
 # Start Supervisor with the specified configuration (using the correct path found during build)
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf 
